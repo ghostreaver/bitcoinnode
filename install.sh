0f0adb9c59ec2de7a8993c4051d2419fd9581a4e
NeosLab <github@neoslab.com>
2024-03-28 17:35:09 -0400

    Update project repository

--- install.sh
+++ install.sh
@@ -1,152 +1,70 @@
-## Bitcoin Pruned Node Server
+#!/bin/bash
 
-Setup for a server hosting a Bitcoin pruned node on Ubuntu 22.04 server freshly installed.
-
-* * *
-
-#### Prepare the environment
-
-Change user password
-
-```shell
+## Change user password
+## --------------------
 passwd ${USER}
-```
 
-Configure APT sources
-
-```shell
+## Configure APT sources
+## ---------------------
 sudo add-apt-repository -y main && sudo add-apt-repository -y restricted && sudo add-apt-repository -y universe && sudo add-apt-repository -y multiverse
-```
 
-Keep system safe
-
-```shell
+## Keep system safe
+## ----------------
 sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
 sudo apt-get -y remove && sudo apt-get -y autoremove
 sudo apt-get -y clean && sudo apt-get -y autoclean
-```
 
-Disable error reporting
-
-```shell
+## Disable error reporting
+## -----------------------
 sudo sed -i "s/enabled=1/enabled=0/" /etc/default/apport
-```
 
-Edit SSH settings
-
-```shell
+## Edit SSH settings
+## -----------------
 sudo sed -i "s/#Port 22/Port 49622/" /etc/ssh/sshd_config
 sudo sed -i "s/#LoginGraceTime 2m/LoginGraceTime 2m/" /etc/ssh/sshd_config
 sudo sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/" /etc/ssh/sshd_config
 sudo sed -i "s/#StrictModes yes/StrictModes yes/" /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
-```
 
-Install prerequisite packages
-
-```shell
+## Install prerequisite packages
+## -----------------------------
 sudo apt-get -y install git build-essential libtool autotools-dev automake pkg-config bsdmainutils python3 libssl-dev libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev
-```
 
-Install Berkeley DB
-
-```shell
+## Install Berkeley DB
+## -------------------
 sudo apt-get -y install libdb5.3++-dev
-```
 
-Clone the Bitcoin Core repository from GitHub.
-
-```shell
+## Clone the Bitcoin Core repository from GitHub.
 cd /home/ubuntu
 git clone https://github.com/bitcoin/bitcoin
-```
 
-Build Bitcoin Core
-
-```shell
+## Build Bitcoin Core
+## ------------------
 cd bitcoin
 ./autogen.sh
 ./configure --enable-cxx --disable-shared --with-pic --prefix=$PWD/depends/x86_64-pc-linux-gnu
 make
-```
 
-Build the necessary dependencies
-
-```shell
+## Build the necessary dependencies
+## --------------------------------
 make -C depends
-```
 
-Configure Bitcoin Core with pruning enabled
-
-```shell
+## Configure Bitcoin Core with pruning enabled
+## -------------------------------------------
 ./configure --with-incompatible-bdb --enable-upnp-default --disable-wallet --disable-bench --disable-tests --disable-gui-tests --disable-gui --disable-zmq --enable-reduce-exports --enable-debug --enable-hardening --enable-werror --with-gui=no --prefix=/usr/local --mandir=/usr/local/share/man --bindir=/usr/local/bin --libdir=/usr/local/lib --libexecdir=/usr/local/lib --disable-dependency-tracking --with-daemon --with-utils --enable-pruning
-```
 
-Build Bitcoin Core with the configuration
-
-```shell
+## Build Bitcoin Core with the configuration
+## -----------------------------------------
 make -j$(nproc)
-```
 
-Install Bitcoin Core
-
-```shell
+## Install Bitcoin Core
+## --------------------
 sudo make install
-```
 
-Initialize the Bitcoin Core data directory
-
-```shell
+## Initialize the Bitcoin Core data directory
+## ------------------------------------------
 bitcoind -daemon
-```
 
-Check synchronization progress
-
-```shell
+## Check synchronization progress
+## ------------------------------
 bitcoin-cli getblockchaininfo
-```
-
-* * *
-
-#### Automated Setup
-
-If you prefer you can uses our automated script using the following command.
-
-```shell
-cd /tmp/ && wget -O - https://raw.githubusercontent.com/ghostreaver/bitcoinnode/main/install.sh | bash
-```
-
-* * *
-
-#### Synchronization
-
-When the synchronization is complete, you can stop Bitcoin Core.
-
-```shell
-bitcoin-cli stop
-```
-
-Open the Bitcoin configuration file
-
-```shell
-nano ~/.bitcoin/bitcoin.conf
-```
-
-Add the following line to enable pruning (replace XXXX with the desired size in MB).
-
-```shell
-prune=XXXX
-```
-
-Start Bitcoin Core again
-
-```shell
-bitcoind -daemon
-```
-
-* * *
-
-#### Conclusion
-
-
-Your pruned Bitcoin node should now be running on Ubuntu 22.04. Make sure to keep your node updated regularly and monitor its performance.
